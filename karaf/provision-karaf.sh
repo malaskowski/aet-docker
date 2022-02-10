a8d8b75a7f78666d6e7013eb0d89b2f2879dde85
Maciej Laskowski <19705838+malaskowski@users.noreply.github.com>
2022-02-10 15:35:48 +0100

    [WIP] Fix Karaf pax.logging dependency (#34)
    Fix Karaf pax logging dependency

--- karaf/provision-karaf.sh
+++ karaf/provision-karaf.sh
@@ -17,6 +17,7 @@
 #
 #!/bin/sh
 
+TOTA_BUNDLES_TO_WAIT=$1
 KARAF_URL=http://karaf:karaf@localhost:8181/system/console/bundles.json
 
 get_status_code() {
@@ -27,20 +28,20 @@ get_bundles_status() {
   curl -s ${KARAF_URL} | jq '.status'
 }
 
-echo "Start waiting for Karaf to load features - up to 200 seconds, waiting for 189 active bundles"
+echo "Start waiting for Karaf to load features - up to 00 seconds, waiting for ${TOTA_BUNDLES_TO_WAIT} active bundles"
 
 /opt/karaf/bin/karaf run &
 
 for i in $(seq 0 60);
 do
   sec=$((5 * $i))
-  if curl -s ${KARAF_URL} 2>&1 | jq '.status' | grep -Fq "189 bundles active";
+  if curl -s ${KARAF_URL} 2>&1 | jq '.status' | grep -Fq "${TOTA_BUNDLES_TO_WAIT} bundles active";
   then
     echo "Karaf loading finished after $sec seconds";
     get_bundles_status
     /opt/karaf/bin/karaf stop
     echo "Status: $(/opt/karaf/bin/karaf status)"
-
+    sleep 10;
     echo "*****  /home/karaf/.m2/repository  ****"
     tree /home/karaf/.m2/repository
     echo "***************************************"
@@ -48,7 +49,7 @@ do
     exit 0;
   else
     status_code=$(get_status_code)
-    echo "Karaf loading from $sec sec... response: $status_code";
+    echo "Karaf loading for $sec sec... response: $status_code | (waiting for ${TOTA_BUNDLES_TO_WAIT} active bundles)";
     if [ "$status_code" -eq 200 ]
     then
       get_bundles_status
